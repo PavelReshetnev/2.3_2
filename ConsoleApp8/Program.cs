b796c8fe7bb602044c68eb008ca0e4b3e5ddd617
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 13:29:08 +0700

    Initial commit

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp8;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Worker tom = new Worker("Tom", "YSACHEV", 2000, 30);
+        tom.Get_Salary();
+    }
+}
---
+++ ConsoleApp8/Worker.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp8;
+
+public class Worker
+{
+    private string Name;
+    private string Surname;
+    private int Rate;
+    private int Days;
+
+    public Worker(string Name, string Surname, int Days, int Rate) {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Days = Days;
+        this.Rate = Rate;
+    }
+    public string name { 
+        get => this.Name; 
+        set => this.Name = value;
+    }
+    public string surname { 
+        get => this.Surname; 
+        set => this.Surname = value;
+    }
+    public int rate { 
+        get => this.Rate; 
+        set => this.Rate = value;
+    }
+    public int days { 
+        get => this.Days; 
+        set => this.Days = value;
+    }
+    
+
+    public void Get_Salary()
+    {
+        int sar = Rate * Days;
+        Console.WriteLine($"{Name} {Surname} заработал {sar} рублей");
+    }
+}
